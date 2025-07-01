6eb8e3d2e091d82674a9d70020fc4e6ee40964d6
mike.francoislubin@gmail.com <mike.francoislubin@gmail.com>
2025-07-01 15:01:35 +0000

    Intermediate orders operational

---
+++ models/intermediate/int_orders_operational.sql
@@ -0,0 +1,13 @@
+{{ config(materialized='table') }}
+
+select 
+int_sales_margin.orders_id
+, int_sales_margin.date_date
+, ROUND(SUM((int_sales_margin.margin + stg_raw__ship.shipping_fee) - (stg_raw__ship.shipment_cost + stg_raw__ship.logcost)), 2) as operational_margin
+from {{ ref('int_sales_margin') }}
+inner join {{ ref('stg_raw__ship') }} 
+using (orders_id)
+group by int_sales_margin.date_date
+, int_sales_margin.orders_id
+order by int_sales_margin.date_date desc
+, int_sales_margin.orders_id desc
