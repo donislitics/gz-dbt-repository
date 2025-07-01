a51f91ac9d52574016eb4c4d026f7ace54a45470
mike.francoislubin@gmail.com <mike.francoislubin@gmail.com>
2025-07-01 15:18:06 +0000

    finance_days

--- models/intermediate/int_orders_operational.sql
+++ models/intermediate/int_orders_operational.sql
@@ -4,6 +4,14 @@ select
 int_sales_margin.orders_id
 , int_sales_margin.date_date
 , ROUND(SUM((int_sales_margin.margin + stg_raw__ship.shipping_fee) - (stg_raw__ship.shipment_cost + stg_raw__ship.logcost)), 2) as operational_margin
+, int_sales_margin.quantity
+, int_sales_margin.purchase_price
+, int_sales_margin.purchase_cost
+, int_sales_margin.revenue
+, int_sales_margin.margin
+, stg_raw__ship.shipping_fee
+, stg_raw__ship.shipment_cost
+, stg_raw__ship.logcost
 from {{ ref('int_sales_margin') }}
 inner join {{ ref('stg_raw__ship') }} 
 using (orders_id)
