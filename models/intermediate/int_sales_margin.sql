8b9f9e8df921f9993ed69b0c73375619bcf8c6d5
mike.francoislubin@gmail.com <mike.francoislubin@gmail.com>
2025-07-01 15:47:38 +0000

    revue calcul sales margin

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -3,11 +3,11 @@
 select 
 stg_raw__sales.date_date
 , stg_raw__sales.orders_id
-, ROUND(SUM(stg_raw__sales.quantity))
-, ROUND(SUM(stg_raw__product.purchase_price))
-, ROUND((SUM(purchase_price) * SUM(quantity)), 2) as purchase_cost
-, ROUND(SUM(stg_raw__sales.revenue))
-, ROUND(SUM(revenue) - (SUM(purchase_price) * SUM(quantity)), 2) as margin
+, ROUND(SUM(stg_raw__sales.quantity)) as quantity
+, ROUND(SUM(stg_raw__product.purchase_price)) as purchase_price
+, ROUND(SUM(purchase_price * quantity), 2) as purchase_cost
+, ROUND(SUM(stg_raw__sales.revenue)) as revenue
+, ROUND(SUM((revenue) - (purchase_price * quantity)), 2) as margin
 from {{ ref('stg_raw__sales') }} 
 inner join {{ ref('stg_raw__product') }} 
 using (products_id)
