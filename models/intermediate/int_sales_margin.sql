3ac5321acda8be14864bd402c4613ffd6ba3db36
mike.francoislubin@gmail.com <mike.francoislubin@gmail.com>
2025-07-01 14:34:02 +0000

    intermediate sales margin

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -1,3 +1,5 @@
+{{ config(materialized='table') }}
+
 select 
 stg_raw__sales.date_date
 , stg_raw__sales.orders_id
@@ -7,6 +9,6 @@ stg_raw__sales.date_date
 , (purchase_price * quantity) as purchase_cost
 , stg_raw__sales.revenue
 , (revenue - (purchase_price * quantity)) as margin
-from {{ ref('stg_raw__sales') }}
+from {{ ref('stg_raw__sales') }} 
 inner join {{ ref('stg_raw__product') }} 
 using (products_id)
